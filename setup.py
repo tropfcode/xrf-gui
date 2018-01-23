0dea6684e78de6dcd296036d775c4ff20c25bfd6
Derek Tropf <derek.tropf@gmail.com>
2018-01-23 15:03:31 -0500

    Remove extra setup.py file

--- setup.py
+++
@@ -1,8 +0,0 @@
-from distutils.core import setup
-setup(
-    name = 'xrfgui',
-    packages = ['xrfgui'],
-    version = '0.0.1',
-    description = 'Vizualization and data manipulation tool for HXN Beamline',
-    author='Derek Tropf',
-)
